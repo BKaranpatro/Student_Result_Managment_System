470b8b259cf702696228ef5ffaaf545226a3f3e2
BKaranpatro <117722113+BKaranpatro@users.noreply.github.com>
2023-04-21 13:43:54 +0530

    Add files via upload

---
+++ student_result_managment_system/DriverManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student_result_managment_system;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author b.karankumarpatro
+ */
+class DriverManager {
+
+    static Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Connection getConnection(String jdbcmysqllocalhost3306SRM, String root, String minimum1998) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Connection getconnection(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Connection getconnection(String jdbcmysqllocalhost_3306SRM, String root, String minimum1998) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ student_result_managment_system/JOptionPan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student_result_managment_system;
+
+/**
+ *
+ * @author b.karankumarpatro
+ */
+class JOptionPan {
+
+    static void showMessageDialog(Object object, String username_or_password_is_incorrect) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
